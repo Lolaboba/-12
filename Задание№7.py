2412d87247fc51a1c9e9141dd87689b0793d83fe
Lolaboba <amongussus15@mail.ru>
2024-10-09 19:25:05 +0300

    Add files via upload

---
+++ Задание№7.py
@@ -0,0 +1,7 @@
+#7 задача
+from random import randint
+qwq=[]
+for i in range(10):
+    qwq.append(randint(1,666))
+qqq = min(qwq)
+print(qqq)
---
+++ Задание№8.py
@@ -0,0 +1,8 @@
+#7 задача
+from random import randint
+S=0
+qwq=[]
+for i in range(10):
+    qwq.append(randint(1,666))
+    S += i
+    print(S)
